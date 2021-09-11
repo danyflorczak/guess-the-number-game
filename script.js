7ece2313aeca5700564fe89fd6a41e8d91fb0ba7
Daniel Florczak <71887549+danyflorczak@users.noreply.github.com>
2021-09-11 20:19:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  //When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'NO number!';
+  }
+  //When player wins
+  else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high!' : 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You Lost The Game ;((');
+    }
+  }
+});
